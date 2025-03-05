c64d9fcf4b47ac7b8ba895c2fd17ab8c53cb4c48
John-Alz <99442406+John-Alz@users.noreply.github.com>
2025-03-04 23:57:26 -0500

    feat: CRUD apirest

---
+++ src/main/java/com/example/crudrapido/entity/Student.java
@@ -0,0 +1,52 @@
+package com.example.crudrapido.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+
+@Entity
+@Table(name = "tbl_student")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long studentId;
+    private String firstName;
+    private String lastName;
+
+    @Column(name = "email_address", unique = true, nullable = false)
+    private String email;
+
+    public Long getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(Long studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
